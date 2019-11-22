e817ba81b6a77eb8b47cc4b092e2616335f4e0fa
David R <deveeup@MacBook-Pro.local>
2019-11-22 16:35:06 -0500

    header menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../assets/styles/components/Header.scss';
-import aerotravel from '../assets/images/aerotravel.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/styles/components/Header.scss";
+import aerotravel from "../assets/images/aerotravel.png";
 
-const Header = (props) => (
+const Header = props => (
   <header>
-    <Link to='/'>
+    <Link to="/">
       <img src={aerotravel} alt="aerotravel" />
     </Link>
     <div className="burguerMenu">
-      <div className="burguerMenu-line"/>
-      <div className="burguerMenu-line"/>
+      <ul>
+        <li>
+          <a href="#">Comprar</a>
+        </li>
+        <li>
+          <a href="#">Vender</a>
+        </li>
+        <li>
+          <a href="#">Promociones</a>
+        </li>
+      </ul>
     </div>
   </header>
 );
